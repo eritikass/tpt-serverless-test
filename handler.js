aa447bf06742ecab2062a36dc0803a9b0d321854
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2020-04-15 14:55:53 +0300

    add some base code for homework

--- handler.js
+++ handler.js
@@ -1,16 +1,27 @@
 'use strict';
+const _ = require('lodash');
 
 module.exports.hello = async event => {
+  /*
   let { queryStringParameters } = event;
   if (!queryStringParameters){
     queryStringParameters = {};
   }
-  
+
   const a = parseInt(event.queryStringParameters.a, 10) || 0;
   const b = parseInt(event.queryStringParameters.b, 10) || 0;
+  */
 
+  const name = (event.queryStringParameters || {}).name || '';
   return {
     statusCode: 200,
+    headers: { 'Content-Type': 'text/html'},
+    body: (name ? `<h1>Hello ${_.escape(name)}!</h1>` : '' ) + `<form action="/dev/hello" method="GET">
+    input: <input type="text" name="name" value="${_.escape(name)}"/>
+    <input type="submit"/>
+    </form>
+    `
+    /*
     body: JSON.stringify(
       {
         a,
@@ -22,6 +33,7 @@ module.exports.hello = async event => {
       null,
       2
     ),
+    */
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration

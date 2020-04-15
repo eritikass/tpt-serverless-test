6a47c63e916a1aedb611840ca65a334b4ac916bb
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2020-04-15 13:56:06 +0300

    Add serverless offline and other lesson work

--- handler.js
+++ handler.js
@@ -1,12 +1,23 @@
 'use strict';
 
 module.exports.hello = async event => {
+  let { queryStringParameters } = event;
+  if (!queryStringParameters){
+    queryStringParameters = {};
+  }
+  
+  const a = parseInt(event.queryStringParameters.a, 10) || 0;
+  const b = parseInt(event.queryStringParameters.b, 10) || 0;
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        a,
+        b,
+        sum: a + b,
+        //message: 'Go Serverless v1.0! Your function executed successfully!',
+        //input: event,
       },
       null,
       2

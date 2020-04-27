4d0b5ec410d91e2136e0c214adb9a900213ba460
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2020-04-27 15:09:14 +0300

    Modify eslint rules

--- tests/unit-tests/equalize.test.js
+++ tests/unit-tests/equalize.test.js
@@ -5,33 +5,33 @@ test('5 + 3 equals 8', () => {
 });
 
 test('15 - 19 equals -4', () => {
-    expect(equalize(15, 19, '-')).toBe(-4);
+  expect(equalize(15, 19, '-')).toBe(-4);
 });
 
 test('15 / 3 equals 5', () => {
-    expect(equalize(15, 3, '/')).toBe(5);
+  expect(equalize(15, 3, '/')).toBe(5);
 });
 
 test('25 * 5 equals 125', () => {
-    expect(equalize(25, 5, '*')).toBe(125);
+  expect(equalize(25, 5, '*')).toBe(125);
 });
 
 test('5 > 3 equals true', () => {
-    expect(equalize(5, 3, '>')).toBe(true);
+  expect(equalize(5, 3, '>')).toBe(true);
 });
 
 test('5 >= 6 equals false', () => {
-    expect(equalize(5, 6, '>=')).toBe(false);
+  expect(equalize(5, 6, '>=')).toBe(false);
 });
 
 test('8 = 8 equals true', () => {
-    expect(equalize(8, 8, '==')).toBe(true);
+  expect(equalize(8, 8, '==')).toBe(true);
 });
 
 test('5 =< 15 equals true', () => {
-    expect(equalize(5, 15, '<=')).toBe(true);
+  expect(equalize(5, 15, '<=')).toBe(true);
 });
 
 test('-15 < -25 equals false', () => {
-     expect(equalize(-15, -25, '<')).toBe(false);
+  expect(equalize(-15, -25, '<')).toBe(false);
 });
--- tests/unit-tests/handler.test.js
+++ tests/unit-tests/handler.test.js
@@ -2,17 +2,17 @@ const equalize = require('../../equalize');
 const handler = require('../../handler');
 
 test('handler with given values returns correct html', () => {
-    expect(handler.hello(async (event) => {
-        const value_1 = 123;
-        const value_2 = 456;
-        const operation = '+';
-      
-        const answer = equalize(value_1, value_2, operation);
-      
-        return {
-          statusCode: 200,
-          headers: { 'Content-Type': 'text/html' },
-          body: `<h3>Awesome Calculator!<h3>
+  expect(handler.hello(async (event) => {
+    const value_1 = 123;
+    const value_2 = 456;
+    const operation = '+';
+
+    const answer = equalize(value_1, value_2, operation);
+
+    return {
+      statusCode: 200,
+      headers: { 'Content-Type': 'text/html' },
+      body: `<h3>Awesome Calculator!<h3>
           <form action="/dev/hello" method="GET" style="margin: 0 auto;">
           <input id="ValueInput1" type="number" name="num_1" placeholder="Value 1" value="${value_1}"/>
       
@@ -33,7 +33,6 @@ test('handler with given values returns correct html', () => {
           <input id="AnswerOutput" type="text" name="answer" placeholder="Answer" value="${answer}" disabled/>
           </form>
           `,
-        };
-    
-      }));
-  });
+    };
+  }));
+});

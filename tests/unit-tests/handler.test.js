370a8f207c42699f9fc81cb5b2d4c07c65f7f0ce
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2020-04-29 12:55:48 +0300

    Fix handler test function.

--- tests/unit-tests/handler.test.js
+++ tests/unit-tests/handler.test.js
@@ -2,7 +2,8 @@ const handler = require('../../handler');
 
 // jest.setTimeout(10000);
 
-test('handler returns correct http response', async (event) => {
+test('handler returns correct http response', async (done) => {
+  const someEvent = new Event('fake');
   const expectedResponse = {
     body: `<h3>Awesome Calculator!<h3>
     <form action="/dev/calc" method="GET" style="margin: 0 auto;">
@@ -29,6 +30,8 @@ test('handler returns correct http response', async (event) => {
     statusCode: 200,
   };
 
-  const response = await handler.calculator(event);
-  expect(response).toEqual(expectedResponse);
+  await handler.calculator(someEvent).then((response) => {
+    expect(response).toEqual(expectedResponse);
+    done();
+  });
 });

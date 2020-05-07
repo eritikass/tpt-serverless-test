3d72d05e6c8b8198a68897519d383db5f5b97406
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2020-05-07 19:36:39 +0300

    Add improvements, more todo

--- handler.js
+++ handler.js
@@ -1,3 +1,4 @@
+const _lodash = require('lodash');
 const equalize = require('./equalize');
 
 module.exports.calculator = async (event) => {
@@ -8,31 +9,32 @@ module.exports.calculator = async (event) => {
 
   const answer = equalize(value_1, value_2, operation);
 
+  const selectedArray = ['', '', '', '', '', '', '', '', ''];
 
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'text/html' },
     body: `<h3>Awesome Calculator!<h3>
-    <form action="/dev/calc" method="GET" style="margin: 0 auto;">
-    <input id="ValueInput1" type="number" name="num_1" placeholder="Value 1" value="${value_1}"/>
-    
-    <select id="SelectOperation" value="${operation}" name="operation">
-      <option value="+"> + </option>
-      <option value="-"> - </option>
-      <option value="*"> * </option>
-      <option value="/"> / </option>
-      <option value=">"> > </option>
-      <option value=">="> >= </option>
-      <option value="=="> = </option>
-      <option value="<="> =< </option>
-      <option value="<"> < </option>
-    </select>
+      <form action="/dev/calc" method="GET" style="margin: 0 auto;">
+      <input id="ValueInput1" type="number" name="num_1" placeholder="Value 1" value="${_lodash.escape(value_1)}"/>
+      
+      <select id="SelectOperation" name="operation">
+        <option value="+" ${selectedArray[0]}> + </option>
+        <option value="-" ${selectedArray[1]}> - </option>
+        <option value="*" ${selectedArray[2]}> * </option>
+        <option value="/" ${selectedArray[3]}> / </option>
+        <option value=">" ${selectedArray[4]}> > </option>
+        <option value=">=" ${selectedArray[5]}> >= </option>
+        <option value="==" ${selectedArray[6]}> = </option>
+        <option value="<=" ${selectedArray[7]}> =< </option>
+        <option value="<" ${selectedArray[8]}> < </option>
+      </select>
 
-    <input id="ValueInput2" type="number" name="num_2" placeholder="Value 2" value="${value_2}"/>
-    <input id="OperationSubmit" type="submit" value="="/>
-    <input id="AnswerOutput" type="text" name="answer" placeholder="Answer" value="${answer}" disabled/>
-    </form>
-    `,
+      <input id="ValueInput2" type="number" name="num_2" placeholder="Value 2" value="${_lodash.escape(value_2)}"/>
+      <input id="OperationSubmit" type="submit" value="="/>
+      <input id="AnswerOutput" type="text" name="answer" placeholder="Answer" value="${_lodash.escape(answer)}" disabled/>
+      </form>
+      `,
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
--- tests/unit-tests/handler.test.js
+++ tests/unit-tests/handler.test.js
@@ -1,37 +1,37 @@
 const handler = require('../../handler');
 
-// jest.setTimeout(10000);
+describe('Handler Tests:', () => {
+  test('handler returns correct http response', async (done) => {
+    const someEvent = new Event('fake');
+    const expectedResponse = {
+      body: `<h3>Awesome Calculator!<h3>
+      <form action="/dev/calc" method="GET" style="margin: 0 auto;">
+      <input id="ValueInput1" type="number" name="num_1" placeholder="Value 1" value="0"/>
+      
+      <select id="SelectOperation" name="operation">
+        <option value="+" > + </option>
+        <option value="-" > - </option>
+        <option value="*" > * </option>
+        <option value="/" > / </option>
+        <option value=">" > > </option>
+        <option value=">=" > >= </option>
+        <option value="==" > = </option>
+        <option value="<=" > =< </option>
+        <option value="<" > < </option>
+      </select>
 
-test('handler returns correct http response', async (done) => {
-  const someEvent = new Event('fake');
-  const expectedResponse = {
-    body: `<h3>Awesome Calculator!<h3>
-    <form action="/dev/calc" method="GET" style="margin: 0 auto;">
-    <input id="ValueInput1" type="number" name="num_1" placeholder="Value 1" value="0"/>
-    
-    <select id="SelectOperation" value="+" name="operation">
-      <option value="+"> + </option>
-      <option value="-"> - </option>
-      <option value="*"> * </option>
-      <option value="/"> / </option>
-      <option value=">"> > </option>
-      <option value=">="> >= </option>
-      <option value="=="> = </option>
-      <option value="<="> =< </option>
-      <option value="<"> < </option>
-    </select>
+      <input id="ValueInput2" type="number" name="num_2" placeholder="Value 2" value="0"/>
+      <input id="OperationSubmit" type="submit" value="="/>
+      <input id="AnswerOutput" type="text" name="answer" placeholder="Answer" value="0" disabled/>
+      </form>
+      `,
+      headers: { 'Content-Type': 'text/html' },
+      statusCode: 200,
+    };
 
-    <input id="ValueInput2" type="number" name="num_2" placeholder="Value 2" value="0"/>
-    <input id="OperationSubmit" type="submit" value="="/>
-    <input id="AnswerOutput" type="text" name="answer" placeholder="Answer" value="0" disabled/>
-    </form>
-    `,
-    headers: { 'Content-Type': 'text/html' },
-    statusCode: 200,
-  };
-
-  await handler.calculator(someEvent).then((response) => {
-    expect(response).toEqual(expectedResponse);
-    done();
+    await handler.calculator(someEvent).then((response) => {
+      expect(response).toEqual(expectedResponse);
+      done();
+    });
   });
 });

3131dcc53ad541f76058d629a5c5edf27338ea83
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-05-08 10:29:47 +0300

    fix handler prettier/eslint problem

--- handler.js
+++ handler.js
@@ -1,16 +1,15 @@
-/* eslint-disable prettier/prettier */
-"use strict";
-const StaticFileHandler = require("serverless-aws-static-file-handler");
-const express = require("express");
-const path = require("path");
+'use strict';
+const StaticFileHandler = require('serverless-aws-static-file-handler');
+const express = require('express');
+const path = require('path');
 const app = express();
 
-const clientFilesPath = path.join(__dirname, "./build/");
+const clientFilesPath = path.join(__dirname, './build/');
 const fileHandler = new StaticFileHandler(clientFilesPath);
-const serverless = require("serverless-http");
+const serverless = require('serverless-http');
 const router = express.Router();
 
-app.get("/date", (req, res) => {
+app.get('/date', (req, res) => {
 	res.json({
 		date: new Date(),
 	});
@@ -24,26 +23,26 @@ app.get("/date", (req, res) => {
 // 	next();
 // });
 // app.use("/dev", express.static(path.join(__dirname, "build")));
-app.get("/dev", (req, res, next) => {
-	if (req.requestContext && req.requestContext.path === "/dev") {
-		express.static(path.join(__dirname, "build"));
-		res.redirect("/dev/");
+app.get('/dev', (req, res, next) => {
+	if (req.requestContext && req.requestContext.path === '/dev') {
+		express.static(path.join(__dirname, 'build'));
+		res.redirect('/dev/');
 		return;
 	}
 	next();
 });
 app.use((req, res, next) => {
-	if (req.requestContext && req.requestContext.path === "/dev") {
-		res.redirect("/dev/");
-		express.static(path.join(__dirname, "build"));
+	if (req.requestContext && req.requestContext.path === '/dev') {
+		res.redirect('/dev/');
+		express.static(path.join(__dirname, 'build'));
 		return;
 	}
 	next();
 });
 
 module.exports.handler = async (event, context) => {
-	express.static(path.join(__dirname, "build"));
+	express.static(path.join(__dirname, 'build'));
 	serverless(app);
-	event.path = "index.html";
+	event.path = 'index.html';
 	return fileHandler.get(event, context);
 };

0953d24cd564c7d8ea7dc50698aae9f6b98aa598
AnderoV <andero.voosalu@tptlive.ee>
2020-08-27 17:49:51 +0300

    add getSelected

---
+++ getSelected.js
@@ -0,0 +1,39 @@
+let getSelected = (operator) => {
+    let selected = 'selected="selected"';
+    let array = ['', '', '', '', '', '', '', '', ''];
+
+    switch (operator) {
+        case '+':
+            array[0] = selected;
+            break;
+        case '-':
+            array[1] = selected;
+            break;
+        case '/':
+            array[2] = selected;
+            break;
+        case '*':
+            array[3] = selected;
+            break;
+        case '>':
+            array[4] = selected;
+            break;
+        case '>=':
+            array[5] = selected;
+            break;
+        case '=':
+            array[6] = selected;
+            break;
+        case '<=':
+            array[7] = selected;
+            break;
+        case '<':
+            array[8] = selected;
+            break;
+        default:
+            break;
+    }
+    return array;
+}
+
+module.exports = { getSelected };
--- handler.js
+++ handler.js
@@ -1,4 +1,5 @@
 const { calculator } = require('./calculator');
+const { getSelected } = require('./getSelected');
 
 module.exports.hello = async (event) => {
   const operator = (event.queryStringParameters || {}).operator || '+';
@@ -7,19 +8,21 @@ module.exports.hello = async (event) => {
 
   const answer = calculator(parseInt(a, 10), parseInt(b, 10), operator);
 
+  let selected = getSelected(operator);
+
   const html = `
   <body><form action="/dev/hello" method="GET">
       <input type="number" name="a" value="${a}">
       <select name="operator">
-        <option selected="selected" value="+">+</option>
-        <option value="-">-</option>
-        <option value="/">/</option>
-        <option value="*">*</option>
-        <option value=">">&gt;</option>
-        <option value=">=">&gt;=</option>
-        <option value="=">=</option>
-        <option value="<=">&lt;=</option>
-        <option value="<">&lt;</option>
+        <option value="+" ${selected[0]}>+</option>
+        <option value="-" ${selected[1]}>-</option>
+        <option value="/" ${selected[2]}>/</option>
+        <option value="*" ${selected[3]}>*</option>
+        <option value=">" ${selected[4]}>&gt;</option>
+        <option value=">=" ${selected[5]}>&gt;=</option>
+        <option value="=" ${selected[6]}>=</option>
+        <option value="<=" ${selected[7]}>&lt;=</option>
+        <option value="<" ${selected[8]}>&lt;</option>
         
       </select>
       <input type="number" name="b" value="${b}">
